303a28adad0cd58dee812677ea429739beb91bd4
Marco Asbreuk <marco@itsasbreuk.nl>
2014-09-24 18:38:41 +0200

    fixed tests

--- itsa.build.js
+++ itsa.build.js
@@ -1,73 +1,73 @@
 /**
- * The Parcela module is an aggregator for all the individual modules that the library uses.
+ * The ITSA module is an aggregator for all the individual modules that the library uses.
  * The developer is free to use it as it is or tailor it to contain whatever modules
  * he/she might need in the global namespace.
  *
  * The modules themselves work quite well independent of this module and can be used
  * separately without the need of them being integrated under one globa namespace.
  *
- * @module Parcela
+ * @module ITSA
 */
 (function (window) {
     "use strict";
     /**
-     * The Parcela class provides the core functionality for the Parcela library
+     * The ITSA class provides the core functionality for the ITSA library
      * and is the root namespace for all the additional modules.
      *
-     * The Parcela class cannot be instantiated.
-     * Instead, the Parcela function takes a configuration object to allow for tailoring of the library.
-     * The Parcela function returns itself to allow for further chaining.
+     * The ITSA class cannot be instantiated.
+     * Instead, the ITSA function takes a configuration object to allow for tailoring of the library.
+     * The ITSA function returns itself to allow for further chaining.
      *
-     * Calling the Parcela function is optional. If the default configuration is acceptable,
-     * the Parcela class can be used directly.
+     * Calling the ITSA function is optional. If the default configuration is acceptable,
+     * the ITSA class can be used directly.
      *
-     * The Parcela name is usually used only once in an application, when configuring it
+     * The ITSA name is usually used only once in an application, when configuring it
      * and when calling the [`ready`](#method_ready) or [`require`](#method_require) methods.
-     * The callback to these two methods provide a reference to Parcela itself as their argument.
-     * These methods allow the developer to rename Parcela to a shorter name, usually `P`,
+     * The callback to these two methods provide a reference to ITSA itself as their argument.
+     * These methods allow the developer to rename ITSA to a shorter name, usually `P`,
      * for use within the local scope.
      *
-     *  Parcela( config )
+     *  ITSA( config )
      *      .require('dialog', 'event', ...)
      *      .then(function (P) {
-     *          // P is an alias of Parcela
+     *          // P is an alias of ITSA
      *      });
      *
      *  // If the default configuration is acceptable, you can simply do:
-     *  Parcela.require('dialog', 'event', ...)
+     *  ITSA.require('dialog', 'event', ...)
      *      .then(function (P) {
-     *          // P is an alias of Parcela
+     *          // P is an alias of ITSA
      *      });
      *
      *  // If extra modules are to be loaded later, you can simply do:
-     *  Parcela( config ).ready
+     *  ITSA( config ).ready
      *      .then(function (P) {
-     *          // P is an alias of Parcela
+     *          // P is an alias of ITSA
      *      });
      *
      *  // And if no configuration is needed:
-     *  Parcela.ready
+     *  ITSA.ready
      *      .then(function (P) {
-     *          // P is an alias of Parcela
+     *          // P is an alias of ITSA
      *      });
      *
      *
      *
-     * @class Parcela
+     * @class ITSA
      * @static
-     * @param config {Object} Configuration options for the Parcela Library
+     * @param config {Object} Configuration options for the ITSA Library
      * @return self {Object}
     */
-    var Parcela = function (config) {
+    var ITSA = function (config) {
         var key;
         for (key in config) {
-            Parcela._config[key] = config[key];
+            ITSA._config[key] = config[key];
         }
-        return Parcela;
+        return ITSA;
     };
     /**
-     * Global configuration properties for the Parcela object.
-     * It can only be set on initialization via the [`Parcela`](#docs-main) function.
+     * Global configuration properties for the ITSA object.
+     * It can only be set on initialization via the [`ITSA`](#docs-main) function.
      *
      * The config is set at a default-configutation
      *
@@ -75,7 +75,7 @@
      * @type Object
      * @private
     */
-    Parcela._config = {
+    ITSA._config = {
         debug: true,
         base: '/components'
     };
@@ -91,9 +91,7 @@
     require('event/event-emitter.js');
     require('event/event-listener.js');
     require('ypromise');
-    require('promise-ext');
-    require('lang-ext');
-    //require('loader');
+    require('extend-js');
 
     /**
     Reference to the `idGenerator` function in [utils](../modules/utils.html)
@@ -109,8 +107,7 @@
     @type function
     @static
     */
-    Parcela.merge(require('utils'));
-
+    ITSA.merge(require('utils'));
 
     /**
      * Reference to the [IO](io.html) object
@@ -118,8 +115,8 @@
      * @type Object
      * @static
     */
-    Parcela.IO = require('io')(window);
-    Parcela.IO.config.merge(io_config);
+    ITSA.IO = require('io')(window);
+    ITSA.IO.config.merge(io_config);
 
     /**
      * [Event](Event.html)-instance
@@ -127,121 +124,24 @@
      * @type Event
      * @static
     */
-    Parcela.Event = Event;
+    ITSA.Event = Event;
 
     // Now we setup `_afterAsyncFn` --> the `timers` module uses this:
     // whenever `async() or `later() is called, it will invoke `_afterAsyncFn` if it is defined
     // By define it in a way that an event is emitted, we make sure the vDOM will be re-rendered.
     // this event cannot be prevented, halted or preventRendered --> if the user wants to prevent
     // vDOM-rendering, the last argument of `async9)` or `later()` should be used.
-    Parcela.Event.defineEvent(EVENT_NAME_TIMERS_EXECUTION)
+    ITSA.Event.defineEvent(EVENT_NAME_TIMERS_EXECUTION)
               .unHaltable()
               .unPreventable()
               .unRenderPreventable()
               .unSilencable();
-    Parcela._afterAsyncFn = function() {
-        console.log('[Parcela]: ', ' emitting '+EVENT_NAME_TIMERS_EXECUTION+' through Parcela._afterAsyncFn()');
+    ITSA._afterAsyncFn = function() {
+        console.log('[ITSA]: ', ' emitting '+EVENT_NAME_TIMERS_EXECUTION+' through ITSA._afterAsyncFn()');
         // emittng a `dummy`-event which will re-render the dDOM:
-        Parcela.Event.emit(EVENT_NAME_TIMERS_EXECUTION);
+        ITSA.Event.emit(EVENT_NAME_TIMERS_EXECUTION);
     };
 
-    /**
-    Reference to the [Parcel](Parcel.html) static class
+    module.exports = ITSA;
 
-    @property Parcel
-    @type Parcel
-    @static
-    */
-    Parcela.Parcel = require('parcel');
-    Parcela.ParcelEv = require('parcel/events.js')(window);
-
-    var vdom = require('virtual-dom')(window);
-    /**
-    Reference to the virtual DOM [render](vDOM.html#method_render) method
-
-    @property render
-    @type Function
-    @static
-    */
-    Parcela.render = vdom.render;
-    Parcela.rootApp = vdom.rootApp;
-    Parcela.Parcel.vNode = vdom.vNode;
-
-    /**
-    Reference to the [routing module](Router.html).
-
-    @property Router
-    @type Function
-    @static
-    */
-    Parcela.Router = require('routing')(window);
-
-
-    module.exports = Parcela;
-
-    var INIT_TIMEOUT = 3000, // timeout within Parcela.ready ought to be finished
-        INIT_TIMEOUT_MSG = 'Initialisation timeout';
-
-
-
-    /**
-     * Returns a promise when the DOM and Parcela library are ready.
-     *
-     * The callback receives a copy of `Parcela` as its only argument so that the developer can use a shorter name within the application.
-     * It is often called simply `P`.
-     *
-     * Calling the Parcela function is optional. If the default configuration is acceptable,
-     * the Parcela class can be used directly.
-     *
-     * @example
-     *  <script>
-     *      Parcela.ready
-     *      .then(function (P) {
-     *          // P is an alias of Parcela
-     *      });
-     *  </script>
-     *
-     * @example
-     *  <script>
-     *      Parcela({base: '/build'}).ready
-     *      .then(function (P) {
-     *          // P is an alias of Parcela
-     *      });
-     *  </script>
-     *
-     * @property ready
-     * @type Promise
-    */
-    Parcela.ready = function (fn) {
-        var initpromise;
-        if (Parcela._ready) {
-            return Parcela._ready;
-        }
-        initpromise = new Promise(function (resolve, reject) {
-            var promisehash = [];
-            // when not initialized within timeout, reject (which can be called allways: resolved promises don't change state)
-            setTimeout(function() {reject(INIT_TIMEOUT_MSG);}, INIT_TIMEOUT);
-            // do the initializationstuff here, when ready, invoke resolve(P);
-            // every async-action can be placed inside 'promisehash'
-            Promise.all(promisehash).then(
-                function() {
-                    resolve(Parcela);
-                },
-                function(err) {
-                    reject(err);
-                }
-            );
-        });
-        if (typeof fn === 'function') {
-            Parcela._ready = initpromise
-                .then(function (P) {
-                    return fn(P);
-                });
-        }
-        else {
-            Parcela._ready = initpromise;
-        }
-        return Parcela._ready;
-    };
-
-})(global.window || require('fake-dom'));
+})(global.window || require('node-win'));
--- tests/index.js
+++ tests/index.js
@@ -1,26 +1,26 @@
 /*global describe, it */
 "use strict";
 var expect = require('chai').expect;
-var Parcela = require('../parcela.js');
+var ITSA = require('../itsa.build.js');
 
 describe('Integration test, are they available where they should?', function () {
 	it('idGenerator', function () {
-		expect(Parcela.idGenerator('integration', 500)).be.equal('integration-500');
+		expect(ITSA.idGenerator('integration', 500)).be.equal('integration-500');
 	});
 	it('typeOf', function () {
-		expect(Parcela.typeOf([])).be.equal('array');
+		expect(ITSA.typeOf([])).be.equal('array');
 	});
 	it('io', function () {
-		expect(Parcela.IO._createXHR).be.a('function');
+		expect(ITSA.IO.request).be.a('function');
 	});
 	it('event', function () {
-		expect(Parcela.Event).be.an('object');
+		expect(ITSA.Event).be.an('object');
 	});
 	it('event-emitter', function () {
-		expect(Parcela.Event.emitter).be.a('function');
+		expect(ITSA.Event.Emitter).be.a('function');
 	});
 	it('event-listener', function () {
-		expect(Parcela.Event.listener).be.an('object');
+		expect(ITSA.Event.Listener).be.an('object');
 	});
 	it('promise-ext', function () {
 		expect(Promise.finishAll).be.a('function');
@@ -29,22 +29,15 @@ describe('Integration test, are they available where they should?', function () 
 		expect(Function.prototype.rbind).be.a('function');
 	});
 	it('timers', function () {
-		expect(Parcela.later).be.a('function');
-	});
-	it('parcel', function () {
-		expect(Parcela.Parcel).be.a('function');
-	});
-	it('vdom', function () {
-		expect(Parcela.Parcel.vNode).be.a('function');
-		expect(Parcela.render).be.a('function');
+		expect(ITSA.later).be.a('function');
 	});
 	it('timers:asyncfunc emitted after async', function (done) {
-		Parcela.Event.after('timers:asyncfunc', function() {
+		ITSA.Event.after('timers:asyncfunc', function() {
 			done();
 		});
 		// calling async() should emit the event 'timers:asyncfunc',
 		// which is a `dummy`-event which will re-render the dDOM
-		Parcela.async(function() {});
+		ITSA.async(function() {});
 	});
 
 });
